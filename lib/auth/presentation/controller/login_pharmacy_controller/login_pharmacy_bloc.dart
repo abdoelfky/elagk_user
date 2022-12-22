02752ab14795ec5c2e4f861a22227ca0790f8f3d
abdoelfky <aelfky66@gmail.com>
2022-12-22 20:11:33 +0200

    3

--- lib/auth/presentation/controller/login_pharmacy_controller/login_pharmacy_bloc.dart
+++ lib/auth/presentation/controller/login_pharmacy_controller/login_pharmacy_bloc.dart
@@ -1,67 +1,9 @@
-import 'dart:async';
-import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
-import '../../../../shared/global/app_colors.dart';
-import '../../../../shared/utils/app__fonts.dart';
-import '../../../../shared/utils/app_constants.dart';
-import '../../../../shared/utils/app_routes.dart';
-import '../../../../shared/utils/enums.dart';
-import '../../../../shared/utils/navigation.dart';
+import 'login_pharmacy_state.dart';
 
-part 'login_pharmacy_event.dart';
+class LoginPharmacyCubit extends Cubit<LoginPharmacyState> {
+  LoginPharmacyCubit() : super(LoginPharmacyInitialState());
+  static LoginPharmacyCubit get(context) => BlocProvider.of(context);
 
-part 'login_pharmacy_state.dart';
-
-class LoginPharmacyBloc extends Bloc<LoginPharmacyEvent, LoginPharmacyState> {
-  final LoginUseCase loginUseCase;
-
-  LoginPharmacyBloc(this.loginUseCase) : super(const LoginPharmacyState()) {
-    on<LoginEvent>(_login);
-  }
-
-  FutureOr<void> _login(
-      LoginEvent event, Emitter<LoginPharmacyState> emit) async {
-    emit(state.copyWith(loginButtonState: ButtonState.loading));
-    final result = await loginUseCase(
-      LoginParameters(
-        email: event.email,
-        password: event.password,
-      ),
-    );
-    result.fold(
-      (l) {
-        emit(
-          state.copyWith(
-            loginRequestState: RequestState.error,
-            loginButtonState: ButtonState.static,
-            loginMessage: l.message,
-          ),
-        );
-        Fluttertoast.showToast(
-          msg: l.message,
-          toastLength: Toast.LENGTH_SHORT,
-          gravity: ToastGravity.SNACKBAR,
-          backgroundColor: AppColors.red,
-          textColor: Colors.white,
-          fontSize: FontSize.s16,
-        );
-      },
-      (r) {
-        emit(
-          state.copyWith(
-            loginRequestState: RequestState.loaded,
-            loginButtonState: ButtonState.static,
-            loginPharmacy: r,
-          ),
-        );
-        navigateFinalTo(
-          context: event.context,
-          screenRoute: Routes.homeDrawerScreen,
-        );
-      },
-    );
-  }
 }
--- lib/auth/presentation/controller/login_pharmacy_controller/login_pharmacy_state.dart
+++ lib/auth/presentation/controller/login_pharmacy_controller/login_pharmacy_state.dart
@@ -1,37 +1,3 @@
-part of 'login_pharmacy_bloc.dart';
+abstract class LoginPharmacyState {}
 
-class LoginPharmacyState extends Equatable {
-  final LoginPharmacy? loginPharmacy;
-  final RequestState loginRequestState;
-  final ButtonState loginButtonState;
-  final String loginMessage;
-
-  const LoginPharmacyState({
-    this.loginPharmacy,
-    this.loginRequestState = RequestState.loading,
-    this.loginButtonState = ButtonState.static,
-    this.loginMessage = AppConstants.empty,
-  });
-
-  LoginPharmacyState copyWith({
-    LoginPharmacy? loginPharmacy,
-    RequestState? loginRequestState,
-    ButtonState? loginButtonState,
-    String? loginMessage,
-  }) {
-    return LoginPharmacyState(
-      loginPharmacy: loginPharmacy ?? this.loginPharmacy,
-      loginRequestState: loginRequestState ?? this.loginRequestState,
-      loginButtonState: loginButtonState ?? this.loginButtonState,
-      loginMessage: loginMessage ?? this.loginMessage,
-    );
-  }
-
-  @override
-  List<Object?> get props => [
-        loginPharmacy,
-        loginRequestState,
-        loginButtonState,
-        loginMessage,
-      ];
-}
+class LoginPharmacyInitialState extends LoginPharmacyState {}
