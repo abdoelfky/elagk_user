489af9eaccebeda8528da78b78aa9bf98f0bdb8f
abdoelfky <aelfky66@gmail.com>
2022-12-28 23:05:40 +0200

    28/12/2022 commit number 5

--- lib/drawer/presentation/screens/elagk_store_screen.dart
+++ lib/drawer/presentation/screens/elagk_store_screen.dart
@@ -1,35 +1,37 @@
-import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
-import 'package:elagk/auth/presentation/components/MainTextFormField.dart';
 import 'package:elagk/auth/presentation/components/screen_background.dart';
-import 'package:elagk/drawer/presentation/components/complaints_components/wide_button_widget.dart';
 import 'package:elagk/drawer/presentation/components/fixed_appbar_widget.dart';
-import 'package:elagk/drawer/presentation/controller/complaints_controller/complaints_cubit.dart';
 import 'package:elagk/home/presentation/components/app_bar_basket_icon.dart';
-import 'package:elagk/shared/components/toast_component.dart';
+import 'package:elagk/pharmacy/presentation/components/OrderByPrescriptionComponents/OrderByPrescriptionContent.dart';
+import 'package:elagk/pharmacy/presentation/components/category_components/categories_and_products_section.dart';
+import 'package:elagk/pharmacy/presentation/components/category_components/item.dart';
+import 'package:elagk/pharmacy/presentation/components/category_components/search_categories.dart';
+import 'package:elagk/pharmacy/presentation/components/pharmacy_components/pharmacy_appBar.dart';
+import 'package:elagk/pharmacy/presentation/components/pharmacy_components/pharmacy_info.dart';
 import 'package:elagk/shared/global/app_colors.dart';
+import 'package:elagk/shared/utils/app_assets.dart';
 import 'package:elagk/shared/utils/app_routes.dart';
 import 'package:elagk/shared/utils/app_strings.dart';
 import 'package:elagk/shared/utils/app_values.dart';
+import 'package:elagk/shared/utils/default_network_image_widget.dart';
 import 'package:elagk/shared/utils/navigation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 class ElagkStoreScreen extends StatelessWidget {
-  const ElagkStoreScreen({Key? key}) : super(key: key);
-  static final _formKey = GlobalKey<FormState>();
-
+  const ElagkStoreScreen({
+    Key? key
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Directionality(
+    return  Directionality(
       textDirection: TextDirection.rtl,
       child: SafeArea(
         child: Scaffold(
-          resizeToAvoidBottomInset: true,
-          appBar: fixedAppBar(
+          appBar:  fixedAppBar(
             context: context,
             title: AppStrings.elagkStore,
-            actionWidget:const AppBarBasketIcon(),
+            actionWidget: const AppBarBasketIcon(),
             onTap: () {
               navigateTo(
                 context: context,
@@ -37,17 +39,71 @@ class ElagkStoreScreen extends StatelessWidget {
               );
             },
           ),
+
+          backgroundColor: AppColors.offWhite,
           body: ScreenBackground(
             child: SingleChildScrollView(
-              padding: const EdgeInsets.all(AppPadding.p10),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  children: [
+              padding: EdgeInsets.all(AppPadding.p20),
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height: AppSize.s170,
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        // Pharmacy photo
+                        Align(
+                          alignment: Alignment.center,
+                          child: Container(
+                            decoration:  BoxDecoration(
+                                borderRadius: BorderRadius.circular(
+                                  AppSize.s8,
+                                ),
+                                gradient:const LinearGradient(
+                                    begin:Alignment.topRight ,
+                                    end:Alignment.bottomRight ,
+                                    colors:
+                                    [
+                                      Color(0xff137e8f),
+                                      Color(0xff059053),
+                                    ])
+                            ),
+                            width: mediaQueryWidth(context),
+                            height: AppSize.s300,
+                            child: Column(
+                              children: [
+                                const SizedBox(
+                                  height: AppSize.s35,
+                                ),
+                                const Center(
+                                  child: Text(
+                                    AppStrings.searchFor,
+                                    style: TextStyle(
+                                        fontSize: 25,color: Colors.white
+                                    ),
+                                  ),
+                                ),
+                                SearchCategoriesWidget(),
+                              ],
+                            ),
 
-                  ],
-                ),
+
+                          ),
+                        ),                   // Pharmacy information
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                      height: mediaQueryHeight(context) / AppSize.s40),
+                  CategoriesAndProductsSection(
+                    firstCategoryName: "categoryName",
+                  ),
+                ],
+
               ),
+
+
             ),
           ),
         ),
